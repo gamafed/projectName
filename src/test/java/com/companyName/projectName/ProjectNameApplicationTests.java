0082bab2fd501ddd74b8dd3bee3fcf4b54352bd2
fedhu <fedhu@gamasys.com.tw>
2023-03-23 13:46:44 +0800

    init

---
+++ src/test/java/com/companyName/projectName/ProjectNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.companyName.projectName;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
