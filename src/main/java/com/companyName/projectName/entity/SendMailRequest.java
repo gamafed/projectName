1e7e65a925b1259dcacf9cac3ad13cbd04fd43d1
fedhu <fedhu@gamasys.com.tw>
2023-05-30 11:48:46 +0800

    //@NotNull：不能为null，但可以为empty //@NotEmpty：不能为null，而且长度必须大于0 //@NotBlank：只能作用在String上，不能为null，而且调用trim()后

--- src/main/java/com/companyName/projectName/entity/SendMailRequest.java
+++ src/main/java/com/companyName/projectName/entity/SendMailRequest.java
@@ -4,6 +4,9 @@ import java.util.List;
 import javax.validation.constraints.NotEmpty;
 
 public class SendMailRequest {
+    //@NotNull：不能为null，但可以为empty
+    //@NotEmpty：不能为null，而且长度必须大于0
+    //@NotBlank：只能作用在String上，不能为null，而且调用trim()后，长度必须大于0
     @NotEmpty
     private String subject;
 
